93b8b60e8d7945261f0d2cc7f100a704cb4af290
unggi@soemardjo.com <unggi@soemardjo.com>
2016-01-18 08:37:05 -0500

    Checking in changes - Dependency Graph Complete

--- BusinessRules/src/main/scala/codegen/DependencyGraph.scala
+++ BusinessRules/src/main/scala/codegen/DependencyGraph.scala
@@ -1,5 +1,41 @@
 package codegen
 
+import java.io.PrintWriter
+
+import org.stringtemplate.v4.AutoIndentWriter
+
+import scala.collection.immutable.HashSet
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+
+trait Node {
+  val successors = new ListBuffer[Node]()
+
+  def name: String = ""
+
+  def isSuccessor(n: Node): Boolean = successors.contains(n.name)
+
+}
+
+
+case class InputNode(override val name: String) extends Node {
+
+}
+
+case class RuleNode(override val name: String) extends Node {
+
+}
+
+case class VariableNode(override val name: String) extends Node {
+
+}
+
+case class OutputNode(override val name: String) extends Node {
+
+}
+
+
 class DependencyGraph {
 
   /**
@@ -16,27 +52,63 @@ class DependencyGraph {
    *
    **/
 
-  trait Node {
 
+  val inputs = new mutable.HashMap[String, Node]()
+  val nodes = new mutable.HashMap[String, Node]()
+
+
+  def addInputs(args: InputNode*): Unit = {
+    for (input <- args) {
+      inputs.put(input.name, input)
+      nodes.put(input.name, input)
+    }
   }
 
-  case class ReferenceNode() extends Node {
+  def addRules(rules: RuleNode*) =
+    for (rule <- rules)
+      nodes.put(rule.name, rule)
 
+  def addVariables(variables: VariableNode*) =
+    for (variable <- variables)
+      nodes.put(variable.name, variable)
+
+
+  def addOutputs(outputs: OutputNode*) =
+    for (output <- outputs)
+      nodes.put(output.name, output)
+
+  def addDependency(from: Node, to: Node) = from.successors.append(to)
+
+  def addDependency(link: Tuple2[Node, Node]) = link._1.successors.append(link._2)
+
+  def isDependency(from: Node, to: Node): Boolean = from.successors.contains(to.name)
+
+  def isDependency(link: Tuple2[Node, Node]) = link._1.successors.contains(link._2)
+
+  val indent = "--"
+
+  def render(pw: AutoIndentWriter): Unit = {
+    val visited = new HashSet[Node]()
+    for (node <- inputs.values) {
+      pw.write(s"Input[${node.name}]\n")
+      pw.pushIndentation(indent)
+      node.successors.foreach(render(_, pw, visited + node))
+      pw.popIndentation
+    }
   }
 
-  case class RuleNode() extends Node {
-
-  }
-
-  case class VariableNode() extends Node {
-
-  }
-
-  def addModelReference(reference: ReferenceNode): Unit = {
-
-  }
-
-  def addDefinition(): Unit = {
-
+  def render(node: Node, pw: AutoIndentWriter, visited: Set[Node]): Unit = {
+    pw.write("> ")
+    node match {
+      case rule: RuleNode => pw.write(s"Rule[${node.name}]\n")
+      case input: InputNode => pw.write(s"Input[${node.name}]\n")
+      case variable: VariableNode => pw.write(s"Variable[${node.name}]\n")
+      case output: OutputNode => pw.write(s"Output[${node.name}]\n")
+      case unknown =>
+        System.err.println(s"Unknown Graph Node type: ${unknown.getClass}")
+    }
+    pw.pushIndentation(indent)
+    node.successors.foreach(render(_, pw, visited + node))
+    pw.popIndentation
   }
 }
